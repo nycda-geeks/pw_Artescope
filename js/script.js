c1856e1a4ccf67641af1f6d84f55165cbf6aed00
Emily Beekhuis <emily.beekhuis@gmail.com>
2016-04-26 16:24:01 +0200

    menu customizedd and masonry gallery plugin added

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+
+	$.fatNav();
+	// Initialize Masonry
+	$('#content').masonry({
+		columnWidth: 320,
+		itemSelector: '.item',
+		isFitWidth: true,
+		isAnimated: !Modernizr.csstransitions
+	}).imagesLoaded(function() {
+		$(this).masonry('reload');
+	});
+
+});
+
+
